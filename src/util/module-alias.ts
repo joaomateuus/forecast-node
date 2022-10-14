ed3ecb55a417491ae2b4517369070644be125f6a
Joao Mateus <joao.amaral@ifam.edu.br>
2022-10-14 12:08:01 -0300

    commiting commit

---
+++ src/util/module-alias.ts
@@ -0,0 +1,12 @@
+//this file is setting the shortcut @ to acess the directories
+
+import * as path from "path";
+import moduleAlias from "module-alias";
+
+const files = path.resolve(__dirname, '../..');
+
+moduleAlias.addAliases({
+    "@src": path.join(files, "src"),
+    "@test": path.join(files, "test")
+});
+
